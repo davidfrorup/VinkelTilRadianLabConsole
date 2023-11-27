de4a439db536a2cc5aeb7c07740c354ecef90ef3
df <df@PROELE82>
2023-11-27 10:07:18 +0100

    Add project files.

---
+++ LabConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("Indtast en vinkel i grader:");
+
+string input = Console.ReadLine();
+
+
+if (Int32.TryParse(input, out int grader))
+{
+    double radians = DegreesToRadians(grader);
+
+    Console.WriteLine($"{grader} grader er {radians} radianer.");
+}
+else
+{
+    Console.WriteLine("Ugyldigt input. Indtast venligst et heltal.");
+}
+        
+        static double DegreesToRadians(int grader)
+{
+    return grader * Math.PI / 180.0;
+}
